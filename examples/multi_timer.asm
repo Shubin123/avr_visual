97b9ec36f1ac94482191efd496d90f9fcf779b9c
Shubin123 <44554264+Shubin123@users.noreply.github.com>
2026-07-09 20:26:17 -0700

    remove static asset hashing, explicit examples
    verified build should work on windows platform originally tested on mac. on a fresh machine the examples files were not being included. I am building this with some material learned in class and had to rewrite the examples.

---
+++ examples/multi_timer.asm
@@ -0,0 +1,56 @@
+; multi_timer.asm
+; Simple public multi-timer example for ATmega2560
+; Cycles through multiple simulated software timers to toggle LEDs on PORTL
+
+.cseg
+.org 0
+
+start:
+    ldi r16, 0xFF
+    sts DDRL, r16     ; Set PORTL to output
+    clr r20           ; Timer 1 counter
+    clr r21           ; Timer 2 counter
+
+loop:
+    inc r20
+    cpi r20, 10
+    brne check_t2
+    clr r20
+    rcall toggle_led_0
+
+check_t2:
+    inc r21
+    cpi r21, 25
+    brne main_delay
+    clr r21
+    rcall toggle_led_1
+
+main_delay:
+    rcall delay
+    rjmp loop
+
+toggle_led_0:
+    lds r16, PORTL
+    ldi r17, 0b00000001
+    eor r16, r17
+    sts PORTL, r16
+    ret
+
+toggle_led_1:
+    lds r16, PORTL
+    ldi r17, 0b00000010
+    eor r16, r17
+    sts PORTL, r16
+    ret
+
+delay:
+    ldi r17, 0x05
+d1: ldi r18, 0xFF
+d2: ldi r19, 0xFF
+d3: dec r19
+    brne d3
+    dec r18
+    brne d2
+    dec r17
+    brne d1
+    ret
